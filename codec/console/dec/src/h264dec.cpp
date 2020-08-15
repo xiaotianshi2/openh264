72cced6963698e6dc0cf9cc0021120b57ecc02ed
xiaotianshi2 <xiaotianshimail2@gmail.com>
2020-08-15 11:02:57 -0700

    fix readPicture when PPS or SPS is present for every picture.

--- codec/console/dec/src/h264dec.cpp
+++ codec/console/dec/src/h264dec.cpp
@@ -146,9 +146,15 @@ int32_t readPicture (uint8_t* pBuf, const int32_t& iFileSize, const int32_t& buf
         if ((++sps_count == 1) && (non_idr_pict_count == 1 || idr_pict_count == 1)) {
           return read_bytes;
         }
+        if (sps_count == 2) {
+          return read_bytes;
+        }
       } else if (nal_unit_type == 8) {
         if (++pps_count == 1 && sps_count == 1) {
           sps_byte_count = int32_t (ptr - pSpsBuf);
+        }
+        if (pps_count == 2) {
+          return read_bytes;
         }
       }
       if (read_bytes >= bytes_available - 4) {
